c6d310bd944074a05e1f0ab039d49c7eaa8f683f
超级轩 <59958757+13215132879@users.noreply.github.com>
2022-09-21 01:27:10 +0800

    Add files via upload

---
+++ hlx.js
@@ -0,0 +1,26 @@
+
+#葫芦侠：超级轩
+
+#我也是小白，学习当中，每一个代码都是呕心沥血
+
+#白嫖是一件不好的事情
+
+
+[rewrite_local]
+
+
+^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/documents\/subscription\/refresh url script-response-body https://raw.githubusercontent.com/13215132879/KFC/main/hlx.js
+[mitm] 
+
+hostname = license.pdfexpert.com
+
+*******************************/
+
+
+var objc = JSON.parse($response.body);
+
+    objc = 
+{"bundleId":"com.readdle.ReaddleDocsIPad","chargingPlatform":"iOS AppStore","receiptId":1505921661000,"originalTransactionId":430001223657482,"inAppStates":[{"type":"subscription","productId":"com.readdle.ReaddleDocsIPad.subscription.month10_allusers","originalTransactionId":430001223657482,"productName":"subscription","isEligibleForIntroPeriod":false,"subscriptionExpirationDate":"01:26 24/09/9999","subscriptionState":"trial","subscriptionAutoRenewStatus":"autoRenewOff","isInGracePeriod":false,"isInBillingRetryPeriod":false,"entitlements":[]},{"type":"custom purchase","productId":"documents6-user","originalTransactionId":"0000","entitlements":[]}],"receiptStatus":"ok","statisticsInfo":{"events":[]}}
+
+
+$done({body : JSON.stringify(objc)});
