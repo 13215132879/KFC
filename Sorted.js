2f76cea7a26d08992f932519ffca1334974d8280
超级轩 <59958757+13215132879@users.noreply.github.com>
2022-09-22 01:51:04 +0800

    Update Sorted.js

--- Sorted.js
+++ Sorted.js
@@ -6,7 +6,7 @@
 
 [rewrite_local]
 
-^https:\/\/api\.sortedapp\.com\/receipts\/verify url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Sorted.js
+^https:\/\/api\.sortedapp\.com\/receipts\/verify url script-response-body https://raw.githubusercontent.com/13215132879/KFC/main/Sorted.js
 
 [mitm] 
 
